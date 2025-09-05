a7007ca435fa3a1ef709e4bac5cb945ea551dea8
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2025-09-05 18:55:46 -0400

    feat: add TextMesh Pro, add Menu start button

---
+++ Assets/_Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class SceneController : MonoBehaviour
+{
+    public Button startButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startButton.onClick.AddListener(LoadNextScene);
+    }
+
+    
+    void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
